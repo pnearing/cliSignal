fbc33bf421805ade6cc56f84d385a2f2ad11a080
Peter Nearing <me@peternearing.ca>
2023-10-30 10:05:56 -0400

    add num_row and num_cols properties.

--- window.py
+++ window.py
@@ -11,16 +11,40 @@ class Window(object):
     """
     Base window class.
     """
-    def __init__(self, window: curses.window, title: str) -> None:
+    def __init__(self, window, title: str) -> None:
         """
         Initialize the window.
-        :param window: curses.window: The curses window object.
+        :param window: curses.window | curses._CursesWindow: The curses window object.
         :param title: The title of the window.
         """
         object.__init__(self)
         self._window: curses.window = window
         self._title: str = title
         num_rows, num_cols = window.getmaxyx()
+        self.num_rows: int = num_rows - 2
+        self.num_cols: int = num_cols - 2
+        self.min_row = 1
+        self.min_col = 1
+        self.max_row = num_rows - 2
+        self.max_col = num_cols - 2
+        return
+
+    def getmaxyx(self) -> tuple[int, int]:
+        """
+        Get the number of rows and columns as a tuple.
+        :return: tuple[int, int]: The first element of the tuple is the number of rows, and the second element is the
+            number of columns.
+        """
+        return self.num_rows, self.num_cols
+
+    def resize(self) -> None:
+        """
+        Recalculate size variables when the window is resized.
+        :return: None
+        """
+        num_rows, num_cols = self._window.getmaxyx()
+        self.num_rows: int = num_rows - 2
+        self.num_cols: int = num_cols - 2
         self.min_row = 1
         self.min_col = 1
         self.max_row = num_rows - 2
