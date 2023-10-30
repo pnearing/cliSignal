77a171c5adcc8036171505f03ec4fce9c344c432
Peter Nearing <me@peternearing.ca>
2023-10-30 08:25:57 -0400

    add min / max row and min / max col properties.

--- window.py
+++ window.py
@@ -20,6 +20,11 @@ class Window(object):
         object.__init__(self)
         self._window: curses.window = window
         self._title: str = title
+        num_rows, num_cols = window.getmaxyx()
+        self.min_row = 1
+        self.min_col = 1
+        self.max_row = num_rows - 2
+        self.max_col = num_cols - 2
         return
 
     def redraw(self) -> None:
@@ -27,10 +32,17 @@ class Window(object):
         Redraw this window.
         :return: None
         """
+        # Clear the window:
         self._window.clear()
+        # Draw a blue and white border:
         self._window.attron(curses.color_pair(1) | curses.A_BOLD)
         self._window.border()
         self._window.attroff(curses.color_pair(1) | curses.A_BOLD)
+        # Add the title:
         add_title_to_win(self._window, self._title, (curses.color_pair(1) | curses.A_BOLD))
+        # Fill the rest of the screen with a blue background.
+        for row in range(self.min_row, self.max_row + 1):
+            for col in range(self.min_col, self.max_col + 1):
+                self._window.addch(row, col, ' ', curses.color_pair(1))
         return
 
